c07f616715c765901fed75e2697e4f2a8872855a
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2016-06-07 12:58:29 -0400

    Use lateinit to avoid duplicate test setup

--- the_core/src/test/java/usecases/CreateTracksTest.kt
+++ the_core/src/test/java/usecases/CreateTracksTest.kt
@@ -6,8 +6,8 @@ import org.junit.Before
 
 class ImportTracksTest {
 
-    var guiSpy = GuiSpy()
-    var fakeTrackRepository = InMemoryTrackRepository()
+    lateinit var guiSpy: GuiSpy
+    lateinit var fakeTrackRepository: TrackRepository
 
     @Before
     fun setUp() {
@@ -17,21 +17,21 @@ class ImportTracksTest {
 
     @Test
     fun create_track_requires_titles() {
-        create_track(guiSpy, fakeTrackRepository, Track(title=""))
+        create_track(guiSpy, fakeTrackRepository, Track(title = ""))
         assertThat(guiSpy.validationFailedCalls).asList().contains("Tracks must have a title")
     }
 
     @Test
     fun sends_an_id_for_the_track_back_to_the_gui() {
-        create_track(guiSpy, fakeTrackRepository, Track(title="Of The Wrist"))
+        create_track(guiSpy, fakeTrackRepository, Track(title = "Of The Wrist"))
         assertThat(guiSpy.createdTrackId).isNotNull()
     }
 
     @Test
     fun tracks_are_unique_by_name() {
-        create_track(guiSpy, fakeTrackRepository, Track(title="Of The Wrist"))
+        create_track(guiSpy, fakeTrackRepository, Track(title = "Of The Wrist"))
         val firstId = guiSpy.createdTrackId
-        create_track(guiSpy, fakeTrackRepository, Track(title="Of The Wrist"))
+        create_track(guiSpy, fakeTrackRepository, Track(title = "Of The Wrist"))
 
         assertThat(guiSpy.validationFailedCalls).asList().contains("Tracks must be unique")
         assertThat(guiSpy.createdTrackId).isEqualTo(firstId)
